49aa63aca8c3190f3a1bc4344bc9c365dc9b038b
himanshukumar4642 <himanshukumar4642@gmail.com>
2020-06-02 22:55:20 +0530

    adding some more variations of LCS

---
+++ Longest Common Subsequence and similar/LongestCommonSubstring.cpp
@@ -0,0 +1,23 @@
+
+
+int LongestCommonSubstring(string X,string Y,int n,int m){
+	int dp[n+1][m+1];
+	int ans=0;
+	for(int i=0;i<=n;i++){
+		for(int j=0;j<=m;j++){
+			if(i==0||j==0)
+			dp[i][j]=0;
+			else{
+				if(x[i-1]==y[j-1]){
+				dp[i][j]=1+dp[i-1][j-1];
+				ans=max(ans,dp[i][j]);
+			}
+				else
+				dp[i][j]=0;
+			}
+		}
+	}
+	
+	return ans;
+}
+
